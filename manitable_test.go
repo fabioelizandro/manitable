cdf74f5d6fd202ffa3eaf5f80583438dc8e2488b
Fabio Elizandro <fabio@elizandro.com.br>
2023-04-23 23:32:45 -0300

    test: does not chain multiple renames for the same column

--- manitable_test.go
+++ manitable_test.go
@@ -26,6 +26,12 @@ func Test_manitable_suite(t *testing.T) {
 		assert.Equal(t, "c-b\nv-1\n", table.String())
 	})
 
+	t.Run("it does not chain multiple renames for the same column", func(t *testing.T) {
+		table := manitable.New(manitable.NewTableSource([]string{"c-a"}, [][]string{{"v-1"}}))
+		table = table.RenameColumn("c-a", "c-b").RenameColumn("c-b", "c-c")
+		assert.Equal(t, "c-b\nv-1\n", table.String())
+	})
+
 	t.Run("it ignores renames for non existing columns", func(t *testing.T) {
 		table := manitable.New(manitable.NewTableSource([]string{"c-a"}, [][]string{{"v-1"}}))
 		table = table.RenameColumn("c-b", "c-c")
