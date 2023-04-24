a742d5d0b1e9798ed893805face392c55e7c887b
Fabio Elizandro <fabio@elizandro.com.br>
2023-04-23 23:28:05 -0300

    test: ignores deletes for non existing columns

--- manitable_test.go
+++ manitable_test.go
@@ -20,7 +20,7 @@ func Test_manitable_suite(t *testing.T) {
 		assert.Equal(t, "c-b,c-c\nv-1,v-2\n", table.String())
 	})
 
-	t.Run("it ignores non existing columns", func(t *testing.T) {
+	t.Run("it ignores renames for non existing columns", func(t *testing.T) {
 		table := manitable.New(manitable.NewTableSource([]string{"c-a"}, [][]string{{"v-1"}}))
 		table = table.RenameColumn("c-b", "c-c")
 		assert.Equal(t, "c-a\nv-1\n", table.String())
@@ -36,6 +36,12 @@ func Test_manitable_suite(t *testing.T) {
 		table := manitable.New(manitable.NewTableSource([]string{"c-a", "c-b", "c-c"}, [][]string{{"v-1", "v-2", "v-3"}}))
 		table = table.DeleteColumn("c-a").DeleteColumn("c-c")
 		assert.Equal(t, "c-b\nv-2\n", table.String())
+	})
+
+	t.Run("it ignores deletes for non existing columns", func(t *testing.T) {
+		table := manitable.New(manitable.NewTableSource([]string{"c-a"}, [][]string{{"v-1"}}))
+		table = table.DeleteColumn("c-b")
+		assert.Equal(t, "c-a\nv-1\n", table.String())
 	})
 
 	t.Run("it adds column", func(t *testing.T) {
