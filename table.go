217a4f4addcf1858e89a74ceb195159b7b87bbca
Fabio Elizandro <fabio.elizandro@indebted.co>
2023-10-29 11:09:25 +1100

    feat: drop columns

--- table.go
+++ table.go
@@ -10,22 +10,36 @@ func NewTable(columns []string, rows [][]string) Table {
 }
 
 func (t Table) Mutate() TableMutation {
-	return newTableMutation(t, []func(column string, value string) string{})
+	return newTableMutation(t, map[int]bool{}, []func(column string, value string) string{})
 }
 
 // TableMutation
 
 type TableMutation struct {
-	table     Table
-	mutations []func(column string, value string) string
+	table         Table
+	mutations     []func(column string, value string) string
+	columnsToDrop map[int]bool
 }
 
-func newTableMutation(table Table, mutations []func(column string, value string) string) TableMutation {
-	return TableMutation{table: table, mutations: mutations}
+func newTableMutation(table Table, columnsToDrop map[int]bool, mutations []func(column string, value string) string) TableMutation {
+	return TableMutation{table: table, columnsToDrop: columnsToDrop, mutations: mutations}
+}
+
+func (m TableMutation) DropColumn(columnName string) TableMutation {
+	for i, column := range m.table.columns {
+		if column == columnName {
+			columnsToDrop := m.columnsToDrop
+			columnsToDrop[i] = true
+
+			return newTableMutation(m.table, columnsToDrop, m.mutations)
+		}
+	}
+
+	return m
 }
 
 func (m TableMutation) Append(f func(column string, value string) string) TableMutation {
-	return newTableMutation(m.table, append(m.mutations, f))
+	return newTableMutation(m.table, m.columnsToDrop, append(m.mutations, f))
 }
 
 func (m TableMutation) Run() Table {
@@ -34,6 +48,10 @@ func (m TableMutation) Run() Table {
 	for i, row := range rows {
 		newRows[i] = make([]string, len(row))
 		for j, value := range row {
+			if m.columnsToDrop[j] {
+				break
+			}
+
 			for _, mutation := range m.mutations {
 				value = mutation(m.table.columns[j], value)
 			}
